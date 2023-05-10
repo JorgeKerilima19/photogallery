da4d168bcc53e78d7fbf2502ede3002fcbfbfa05
JorgeMioska <quispechilojorge@gmail.com>
2023-05-09 21:42:03 -0500

    first try materialUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import React from "react";
 
+import Button from '@mui/material/Button';
+
 const App = () => {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Button variant="contained">Click Me</Button>
+    </>
+  );
 };
 export default App;
