696d595758acf73062791c1babcab585285c81f6
JorgeMioska <quispechilojorge@gmail.com>
2023-05-10 21:31:20 -0500

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
 import React from "react";
 
-import Button from '@mui/material/Button';
+import AppBar from '@mui/material/AppBar';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
 
 const App = () => {
   return (
     <>
-      <h1>Hello World</h1>
-      <Button variant="contained">Click Me</Button>
+      <AppBar position="fixed" color="primary">
+        <Container>
+          <MenuIcon onClick={()=>{
+            console.log("object");
+          }}/>
+          <Typography variant="h4">Welcome to my photo Gallery</Typography>
+        </Container>
+        
+
+      </AppBar>
     </>
   );
 };
