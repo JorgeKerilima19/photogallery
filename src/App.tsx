843cc7dbbfd9cf0cce9a698a6d90c4800a72b1ab
JorgeMioska <quispechilojorge@gmail.com>
2023-05-12 05:55:15 -0500

    better understaning of the framework

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,39 @@
 import React from "react";
 
-import AppBar from '@mui/material/AppBar';
-import MenuIcon from '@mui/icons-material/Menu';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
+import {
+  Typography,
+  AppBar,
+  Toolbar,
+  Container,
+  CssBaseline,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 const App = () => {
   return (
     <>
-      <AppBar position="fixed" color="primary">
-        <Container>
-          <MenuIcon onClick={()=>{
-            console.log("object");
-          }}/>
+      <CssBaseline />
+      <AppBar position="relative" color="primary">
+        <Toolbar>
+          <MenuIcon
+            onClick={() => {
+              console.log("object");
+            }}
+          />
           <Typography variant="h4">Welcome to my photo Gallery</Typography>
-        </Container>
-        
-
+        </Toolbar>
       </AppBar>
+      <main>
+        <Container>
+          <Typography variant="h5" align="center">
+            Presentation:
+          </Typography>
+          <Typography variant="body1" align="center">
+            I just want to know the world better and there is not better way
+            than traveling within it
+          </Typography>
+        </Container>
+      </main>
     </>
   );
 };
