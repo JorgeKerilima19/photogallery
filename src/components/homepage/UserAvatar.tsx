57f67c1fbed2478289e54b61d9a69b949ec0ec6f
JorgeMioska <quispechilojorge@gmail.com>
2024-02-01 13:34:06 -0500

    story border

--- src/components/homepage/UserAvatar.tsx
+++ src/components/homepage/UserAvatar.tsx
@@ -1,5 +1,7 @@
 import { Avatar, Box, Typography } from "@mui/material";
 
+import img from "/assets/photo1.jpg";
+
 export const UserAvatar = () => {
   return (
     <Box
@@ -9,7 +11,14 @@ export const UserAvatar = () => {
       gap={1}
       padding="0 0.4rem"
     >
-      <Avatar />
+      <Avatar
+        src={img}
+        sx={{
+          // height: "3.5rem",
+          width: "3.5rem",
+          border: "2px solid #7ed957",
+        }}
+      />
       <Typography variant="caption">User</Typography>
     </Box>
   );
